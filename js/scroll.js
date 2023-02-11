badaa7659db8fbbe931f77549d857cc4e3169955
dori-dev <mr.dori.dev@gmail.com>
2023-02-11 16:00:14 +0330

    add navbar scroll to section

---
+++ js/scroll.js
@@ -0,0 +1,7 @@
+const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: ".navbar-nav",
+});
+
+const scrollSpyMobile = new bootstrap.ScrollSpy(document.body, {
+  target: ".nav-mobile",
+});
