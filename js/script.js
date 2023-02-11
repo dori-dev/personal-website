cff5a9dbb5b5bcc16469c120b8d17e5dab158451
dori-dev <mr.dori.dev@gmail.com>
2023-02-11 16:46:55 +0330

    add back to top

--- js/script.js
+++ js/script.js
@@ -22,13 +22,7 @@ if (alertTrigger) {
   });
 }
 
-$(document).ready(function () {
-  $("#success-alert").hide();
-  $("#myWish").click(function showAlert() {
-    $("#success-alert")
-      .fadeTo(2000, 500)
-      .slideUp(500, function () {
-        $("#success-alert").slideUp(500);
-      });
-  });
-});
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
--- js/scroll.js
+++
@@ -1,7 +0,0 @@
-const scrollSpy = new bootstrap.ScrollSpy(document.body, {
-  target: ".navbar-nav",
-});
-
-const scrollSpyMobile = new bootstrap.ScrollSpy(document.body, {
-  target: ".nav-mobile",
-});
